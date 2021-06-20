d6b42ffdfbde7fe337c541d315cc109877a98423
dattran1232003 <dattran1232003@gmail.com>
2021-06-20 15:34:51 +0700

    Design trello layout

---
+++ src/App.jsx
@@ -0,0 +1,101 @@
+import './App.scss'
+
+function App() {
+  return (
+    <div className="master">
+      <nav className="navbar app">App Bar</nav>
+      <nav className="navbar board">Board Bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>Brainstorm</header>
+
+          <ul>
+            <li>
+              <img
+                src="https://dattran1232003.github.io/my-cv/images/profile_image.jpg"
+                alt="dattran image"
+              />
+              Title: DATTRANQUOC
+            </li>
+            <li>Add what you{`'`}d like to work on below</li>
+            <li>Add what you{`'`}d like to work on below</li>
+            <li>Add what you{`'`}d like to work on below</li>
+            <li>Add what you{`'`}d like to work on below</li>
+            <li>Add what you{`'`}d like to work on below</li>
+          </ul>
+
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+
+          <ul>
+            <li>
+              <img
+                src="https://dattran1232003.github.io/my-cv/images/profile_image.jpg"
+                alt="dattran image"
+              />
+              Title: DATTRANQUOC
+            </li>
+            <li>Add what you{`'`}d like to work on below</li>
+          </ul>
+
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+
+          <ul>
+            <li>
+              <img
+                src="https://dattran1232003.github.io/my-cv/images/profile_image.jpg"
+                alt="dattran image"
+              />
+              Title: DATTRANQUOC
+            </li>
+            <li>Add what you{`'`}d like to work on below</li>
+          </ul>
+
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+
+          <ul>
+            <li>
+              <img
+                src="https://dattran1232003.github.io/my-cv/images/profile_image.jpg"
+                alt="dattran image"
+              />
+              Title: DATTRANQUOC
+            </li>
+            <li>Add what you{`'`}d like to work on below</li>
+          </ul>
+
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+
+          <ul>
+            <li>
+              <img
+                src="https://dattran1232003.github.io/my-cv/images/profile_image.jpg"
+                alt="dattran image"
+              />
+              Title: DATTRANQUOC
+            </li>
+            <li>Add what you{`'`}d like to work on below</li>
+          </ul>
+
+          <footer>Add another card</footer>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
