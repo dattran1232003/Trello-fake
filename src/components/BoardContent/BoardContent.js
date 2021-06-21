10f7d8270511d29bbaa5266e40d757b35626672f
dattran1232003 <dattran1232003@gmail.com>
2021-06-21 19:03:37 +0700

    re structure project

---
+++ src/components/BoardContent/BoardContent.js
@@ -0,0 +1,14 @@
+import './BoardContent.scss'
+
+import Column from 'components/Column/Column'
+
+function BoardContent() {
+  return (
+    <div className="board-content">
+      <Column />
+      <Column />
+    </div>
+  )
+}
+
+export default BoardContent
